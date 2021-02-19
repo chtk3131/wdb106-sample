297cac2907927dc9bd1b000c28ebe8507a69885d
chtk3131 <takuya-ochiai@aeroasahi.co.jp>
2021-02-19 09:36:48 +0900

    ・デフォルトで存在していたテストを削除 ・Appクラスを作成し、表示するところまで

---
+++ lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class App extends HookWidget {
+  const App({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+    // return MaterialApp();
+  }
+}
