e1c00912c5478007c7d317c7861531dcd7bd4ddf
chtk3131 <takuya-ochiai@aeroasahi.co.jp>
2021-02-19 09:55:58 +0900

    勝因一覧ページのbody部分を別クラスで定義

--- lib/pages/items_page/items_page.dart
+++ lib/pages/items_page/items_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 class ItemsPage extends StatelessWidget {
   const ItemsPage({Key key}) : super(key: key);
@@ -15,8 +16,19 @@ class ItemsPage extends StatelessWidget {
             onPressed: null,
           ),
         ),
-        body: Container(),
+        body: _ListView(),
       ),
+    );
+  }
+}
+
+class _ListView extends HookWidget {
+  const _ListView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(),
     );
   }
 }
