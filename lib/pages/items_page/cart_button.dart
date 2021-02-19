332dc42a44c2d3b3123961e3c8e32efe040042ac
chtk3131 <takuya-ochiai@aeroasahi.co.jp>
2021-02-19 11:05:41 +0900

    カート内の商品数がボタン名に反映されるようにした

--- lib/pages/items_page/cart_button.dart
+++ lib/pages/items_page/cart_button.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:wdb106_sample/model/controllers/cart_controller/cart_controller.dart';
+import '../../components/components.dart';
 
 // カートに遷移するボタン
 // カートに入っている商品数を表示するため、状態管理が必要
@@ -9,8 +12,11 @@ class CartButton extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(Icons.add),
+    // カート内のアイテム数を取得
+    final cartItemCountText =
+        useProvider(cartProvider.state.select((value) => value.summary.state));
+    return NavigationBarButton(
+      text: cartItemCountText,
       onPressed: null,
     );
   }
