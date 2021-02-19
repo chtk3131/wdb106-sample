f8bc192d9115c41b6304406ac0d86c7fb730bcfd
chtk3131 <takuya-ochiai@aeroasahi.co.jp>
2021-02-19 09:51:14 +0900

    起動時の表示ページ（アイテム一覧ページ）のプロトタイプを表示できるようにした

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:wdb106_sample/pages/items_page/items_page.dart';
 
 class App extends HookWidget {
   const App({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
-    // return MaterialApp();
+    return MaterialApp(
+      navigatorKey: GlobalKey(),
+      home: const ItemsPage(),
+    );
   }
 }
---
+++ lib/pages/items_page/items_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ItemsPage extends StatelessWidget {
+  const ItemsPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Title'),
+        ),
+        body: Container(),
+      ),
+    );
+  }
+}
