a077574ce6757198995f32c2264d8487da81e8f1
chtk3131 <takuya-ochiai@aeroasahi.co.jp>
2021-02-19 10:36:49 +0900

    商品一覧ページのざっくりとした構成はつくった

--- lib/pages/items_page/items_page.dart
+++ lib/pages/items_page/items_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:wdb106_sample/model/controllers/controllers.dart';
 
 class ItemsPage extends StatelessWidget {
   const ItemsPage({Key key}) : super(key: key);
@@ -27,8 +29,20 @@ class _ListView extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(),
-    );
+    // 商品データのロード中か確認
+    final isLoading =
+        useProvider(itemsProvider.state.select((value) => value.isLoading));
+    // 商品のIDリストを取得
+    final ids = useProvider(itemsProvider.state
+        .select((value) => value.stocks.map((e) => e.item.id)));
+
+    return isLoading
+        ? const Center(
+            child: CircularProgressIndicator(),
+          )
+        : ListView.builder(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            itemCount: ids.length,
+            itemBuilder: (_, index) {});
   }
 }
